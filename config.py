13ee2ec8aae3712621c2700691ebfa2f01e84263
HamzaJbili <hamzajbili.ing@gmail.com>
2025-04-09 16:17:07 +0100

    config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+CREDENTIALS_FILE = "users.json"
+SETTINGS_FILE = "settings.json"
+DATA_FILE = "waste_data.csv"
+STRIPE_API_KEY = "sk_test_XXXXXXXXXXXXXXXXXXXXXXXX"  # Replace with your key
